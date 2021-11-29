5e6a0be8613a2501818dde37495b5c215696fd61
qtpham_dev <53518651+QuocToan2402@users.noreply.github.com>
2021-11-29 21:39:10 +0700

    update product

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -30,10 +30,7 @@ export const productListReducer = (
 };
 
 //Same list product action
-export const productDetailsReducer = (
-  state = { product: {}, loading: true },
-  action
-) => {
+export const productDetailsReducer = (state = { loading: true }, action) => {
   switch (action.type) {
     case PRODUCT_DETAILS_REQUEST:
       return { loading: true };
